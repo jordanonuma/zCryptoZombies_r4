fee702f5a6cf790946ac5fc33d8436c0e0a4db4d
Jordan <jordanonuma@outlook.com>
2019-07-28 08:39:27 -1000

    Added struct 'Zombie' containing name and DNA properties.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.4.25;
 contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 } //end contract zombiefactory {}
