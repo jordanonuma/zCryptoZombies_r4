86ac615453126898bda47d363e73d2ecb0676546
Jordan <jordanonuma@outlook.com>
2019-08-19 06:29:51 -1000

    Added import and inheritance statements.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.25;
+import "./Ownable.sol";
 
-contract ZombieFactory {
+contract ZombieFactory is Ownable {
     event NewZombie(uint zombieId, string name, uint dna);
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
