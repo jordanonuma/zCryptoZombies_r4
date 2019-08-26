1e1eaffbb31673f754d6f0ed41287099826a399e
Jordan <jordanonuma@outlook.com>
2019-08-26 06:38:44 -1000

    Added frame for function changeName() that allows the user to change their zombie's name. Minimimum condition is the zombie is level 2.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -8,4 +8,8 @@ contract ZombieHelper is ZombieFeeding {
         require(zombies[_zombieId].level >= _level);
         _;
     } //end modifier aboveLevel()
+
+    function changeName (uint _zombieId, stirng _newName) external aboveLevel(2, _zombieId) {
+    
+    } //end function changeName()
 } //end contract ZombieHelper {}
