51c19a073c88878de7388152f68a56b018d0d605
Jordan <jordanonuma@outlook.com>
2019-08-28 06:24:51 -1000

    Added call to return array at end of function.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -21,5 +21,6 @@ contract ZombieHelper is ZombieFeeding {
 
     function getZombiesByOwner(address _owner) external view returns(uint[]) {
         uint[] memory result = new uint[](ownerZombieCount[_owner]);
+        return result;
     } //end function getZombiesByOwner()
 } //end contract ZombieHelper {}
