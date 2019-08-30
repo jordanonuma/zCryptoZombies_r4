cee6f6a5aeb1f633315aadda4c75c6d0ff086c87
Jordan <jordanonuma@outlook.com>
2019-08-30 05:45:34 -1000

    Added frame for function levelUp() that allows user to increase their zombie's level by one.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -10,6 +10,10 @@ contract ZombieHelper is ZombieFeeding {
         _;
     } //end modifier aboveLevel()
 
+    function levelUp(uint _zombieId) external payable {
+    
+    } //end function levelUp()
+
     function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].name = _newName;
