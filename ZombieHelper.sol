e8b52964c6342c84c1a3fc0259d6ea66744a6a5c
Jordan <jordanonuma@outlook.com>
2019-08-30 05:46:31 -1000

    Added function requirement that user sent the appropriate fee amount.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -11,7 +11,7 @@ contract ZombieHelper is ZombieFeeding {
     } //end modifier aboveLevel()
 
     function levelUp(uint _zombieId) external payable {
-    
+        require(msg.value == levelUpFee);
     } //end function levelUp()
 
     function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
