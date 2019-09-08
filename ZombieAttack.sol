f4bc87ec3847b0802004a189414b9d38443cfa8e
Jordan <jordanonuma@outlook.com>
2019-09-08 12:27:22 -1000

    Revised parameters calling feedAndMultiply().

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -18,7 +18,7 @@ contract ZombieAttack is ZombieHelper {
             myZombie.winCount++;
             myZombie.level++;
             enemyZombie.lossCount++;
-            feedAndMultiply(_zombieId, _targetId, "zombies");
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
         } //end if()
     } //end function attack()
 } //end contract ZombieAttack{}
