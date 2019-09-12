117bcd90f9d63816eb7ac4abd97d5961fbbdff44
Jordan <jordanonuma@outlook.com>
2019-09-12 06:37:59 -1000

    Added frame for functions that return user's balance of zombies and whose the owner of a specific zombie.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -3,5 +3,11 @@ import "./ZombieAttack.sol";
 import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+    function balanceOf(address _owner) external view returns (uint256) {
     
+    } //end function balanceOf()
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+    
+    } //end function ownerOf()
 } //end contract ZombieOwnership{}
