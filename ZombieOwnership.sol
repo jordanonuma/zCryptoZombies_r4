155c284f1b0387165cbff84f9a6d545e99b00a9d
Jordan <jordanonuma@outlook.com>
2019-09-12 06:38:55 -1000

    Added mappings that will return totals or the owner.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -4,10 +4,10 @@ import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
     function balanceOf(address _owner) external view returns (uint256) {
-    
+        return ownerZombieCount[_owner];
     } //end function balanceOf()
 
     function ownerOf(uint256 _tokenId) external view returns (address) {
-    
+        return zombieToOwner[_tokenId];
     } //end function ownerOf()
 } //end contract ZombieOwnership{}
