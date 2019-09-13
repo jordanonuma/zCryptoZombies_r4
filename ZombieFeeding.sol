fde9830113d9d2d3c8f1e7bf3f32edbd50c7d600
Jordan <jordanonuma@outlook.com>
2019-09-13 06:00:55 -1000

    Refactored modifier so that the name isn't the same name as the ERC721 function ownerOf().

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -18,10 +18,10 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract;
-    modifier ownerOf(uint _zombieId) {
+    modifier onlyOwnerOf(uint _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         _;
-    } //end modifier ownerOf()
+    } //end modifier onlyOwnerOf()
 
     function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
@@ -35,7 +35,7 @@ contract ZombieFeeding is ZombieFactory {
         return (_zombie.readyTime <= now);
     } //end function _isReady()
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
