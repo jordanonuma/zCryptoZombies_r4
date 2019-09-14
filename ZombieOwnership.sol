e901fb483c4bd4bdcc3c69af290b8c05fd72a4a7
Jordan <jordanonuma@outlook.com>
2019-09-14 10:09:10 -1000

    Within _transfer() function, updated the zombie ownership mappings and fired the ERC721 Transfer() function so that the front end can update.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -12,6 +12,9 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     } //end function ownerOf()
 
     function _transfer(address _from, address _to, uint256 _tokenId) private {
-    
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
     } //end function _transfer()
 } //end contract ZombieOwnership{}
