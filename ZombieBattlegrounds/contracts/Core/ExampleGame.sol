fb8e9ec546ecec8fd861dcbdc3b8707bd2ef7405
Jordan <jordanonuma@outlook.com>
2019-10-01 06:14:12 -1000

    Added frame for function that captures the state of the game before the battle starts.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -3,5 +3,7 @@ pragma solidity 0.4.25;
 import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
-
-}
+    function beforeMatchStart(bytes serializedGameState) external {
+        
+    }  //end function beforeMatchStart()
+} //end contract ExampleGame {}
