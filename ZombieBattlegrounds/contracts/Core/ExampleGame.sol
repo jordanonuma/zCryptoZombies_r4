dbc4494e734f1f8f575b62d62492a3df96901592
Jordan <jordanonuma@outlook.com>
2019-10-02 06:26:03 -1000

    Declared a new GameState[] array 'gameState' and initialized it.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -4,6 +4,7 @@ import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
     function beforeMatchStart(bytes serializedGameState) external {
-        
+        GameState memory gameState;
+        gameState.init(serializedGameState);
     }  //end function beforeMatchStart()
 } //end contract ExampleGame {}
