2c48f76f55af45964691af8c76ec6eca5df5ec97
Jordan <jordanonuma@outlook.com>
2019-10-03 06:15:49 -1000

    Set both players' initial defense levels to 15.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -6,5 +6,8 @@ contract ExampleGame is ZBGameMode  {
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
         gameState.init(serializedGameState);
+
+        changes.changePlayerDefense(Player.Player1, 15);
+        changes.changePlayerDefense(Player.Player2, 15);
     }  //end function beforeMatchStart()
 } //end contract ExampleGame {}
