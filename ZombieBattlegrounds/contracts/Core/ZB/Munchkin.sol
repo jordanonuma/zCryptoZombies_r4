9d51993910316c2b49220451d16d5474cfc80004
jordanonuma <jordanonuma@outlook.com>
2019-10-12 12:02:53 +0900

    Added logic of nested for() loops that goes throug each player's cards.

--- ZombieBattlegrounds/contracts/Core/ZB/Munchkin.sol
+++ ZombieBattlegrounds/contracts/Core/ZB/Munchkin.sol
@@ -12,6 +12,19 @@ contract Munchkin is ZBGameMode  {
         changes.init();
 
         // Custom game logic will go here
+        for(uint i=0; i < gameState.playerStates.length; i++) {
+            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+            uint cardCount = 0;
+
+            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                if(isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                } //end if()
+            } //end for(j)
+
+            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount); //calls solidity function changePlayerCardsInDeck()
+        } //end for(i)
 
         changes.emit();
     } //end function beforeMatchStarts()
