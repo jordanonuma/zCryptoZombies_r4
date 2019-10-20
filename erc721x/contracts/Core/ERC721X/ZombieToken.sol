ec73e71b4928823ec04391c67bb9cc79eeec4fa2
jordanonuma <jordanonuma@outlook.com>
2019-10-20 08:57:55 -1000

    Assigned the name.

--- erc721x/contracts/Core/ERC721X/ZombieToken.sol
+++ erc721x/contracts/Core/ERC721X/ZombieToken.sol
@@ -4,7 +4,7 @@ import "./ERC721XToken.sol";
 contract ZombieCard is ERC721XToken {
     
     function name() external view returns(string) {
-      
+      return "ZombieCard";
     }  //end function name()
 
 } //end contract ZombieCard {}
