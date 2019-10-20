c6572b99096980b9778514b6ed539a7ffdad3889
jordanonuma <jordanonuma@outlook.com>
2019-10-20 08:59:51 -1000

    Assigned symbol.

--- erc721x/contracts/Core/ERC721X/ZombieToken.sol
+++ erc721x/contracts/Core/ERC721X/ZombieToken.sol
@@ -4,10 +4,10 @@ import "./ERC721XToken.sol";
 contract ZombieCard is ERC721XToken {
     
     function name() external view returns(string) {
-      return "ZombieCard";
+        return "ZombieCard";
     }  //end function name()
 
     function symbol() external view returns(string) {
-
+        return "ZCX";
     } //end function symbol()
 } //end contract ZombieCard {}
