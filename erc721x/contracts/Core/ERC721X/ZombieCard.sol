bbd74a2d04bb02245fe9c7acfadfb284ee5d516c
jordanonuma <jordanonuma@outlook.com>
2019-10-26 10:24:22 -1000

    Subtracted the number of tokens to be converted to non-fungible tokens from the number of fungible tokens.

--- erc721x/contracts/Core/ERC721X/ZombieCard.sol
+++ erc721x/contracts/Core/ERC721X/ZombieCard.sol
@@ -40,5 +40,7 @@ contract ZombieCard is ERC721XToken {
     function convertToNFT(uint _tokenId, uint _amount) public {
         require(tokenType[_tokenId] == FT);
         require(_amount <= balanceOf(msg.sender, _tokenId), "You do not own enough tokens");
+        _updateTokenBalance(msg.sender, _tokenId, _amount, ObjectLib.Operations.SUB);
+        
     } //end function convertToNFT()
 } //end contract ZombieCard {}
